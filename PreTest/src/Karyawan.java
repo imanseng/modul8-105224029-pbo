8801d1ca55df232f5d97411a037c7247440e17b8
imanseng <imandwisatrio99@gmail.com>
2026-05-12 14:13:39 +0700

    kirim PreTest

---
+++ PreTest/src/Karyawan.java
@@ -0,0 +1,8 @@
+public abstract class Karyawan {
+    protected String nama;
+
+    public Karyawan(String nama) {
+        this.nama = nama;
+    }
+    abstract double hitungGaji();
+}
---
+++ PreTest/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Programmer programmer = new Programmer("Budi", 1000000.0);
+        System.out.println(programmer.hitungGaji());
+    }
+}
